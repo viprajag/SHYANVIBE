1e6f808ec9d8ed9e262403b07217212519d4417f
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-08-31 17:08:50 +0530

    Update call.py

--- BIGFM/core/call.py
+++ BIGFM/core/call.py
@@ -5,7 +5,11 @@ from typing import Union
 
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
-from pytgcalls import PyTgCalls, StreamType
+#from pytgcalls import PyTgCalls, StreamType
+from pytgcalls import PyTgCalls
+from pytgcalls.types import StreamType
+
+
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
