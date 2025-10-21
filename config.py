08a37837e575e53bbcbb60785f4b223d15ea25e4
viprajag <viprajatg@gmail.com>
2025-10-21 17:29:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 # 🔄 Git & Update Settings
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/TANYA-SINGH-VNS-UP/SHYANVIBE")
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/viprajag/SHYANVIBE")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
